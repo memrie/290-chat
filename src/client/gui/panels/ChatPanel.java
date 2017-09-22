87b156cdc2763b8c0f8cb67255b716d46c2fd60f
Erika Tobias <et5392@rit.edu>
2017-09-22 10:29:31 -0400

    GUI and Control Updates
    Updated the GUI to be easier to see the controls (more padding) and also updated the functions and their comments.

--- src/client/gui/panels/ChatPanel.java
+++ src/client/gui/panels/ChatPanel.java
@@ -1,19 +1,16 @@
 
-
+/////////////// Directory
 package gui.panels;
 
 /////////////// Event Libraries         
 import java.awt.*;
 import java.awt.event.*;
 
-/////////////// 
-import java.util.*;
-import java.time.*;
-import java.text.*;
-
 /////////////// General Java Libraries
 import java.util.*;
 import java.io.*;
+import java.time.*;
+import java.text.*;
 
 ////////////// GUI Libraries
 import javax.swing.*; 
@@ -40,40 +37,54 @@ import javax.swing.UIManager;
 
 public class ChatPanel{
 	
+	//wrapper panels
 	private JPanel wrapper = new JPanel(new BorderLayout());
 	private JPanel your_message_panel = new JPanel(new BorderLayout());
-	private JPanel button_container = new JPanel(new FlowLayout());
+	private JPanel button_container = new JPanel(new BorderLayout());
 	
-	
-	
-	
+	//message controls
 	private JTextArea messagesList = new JTextArea();
 	private JLabel yourMessageLbl =  new JLabel("Your Message");
-	private JTextArea yourMessageTxt = new JTextArea();
+	private JTextArea yourMessageTxt = new JTextArea(5,5);
 	private JButton sendMessage = new JButton("send");
 	
+	//scroll wrapper panels
 	private JScrollPane message_list_scroll = new JScrollPane(messagesList);
 	private JScrollPane message_scroll = new JScrollPane(yourMessageTxt);
 	
-	
+	/**
+	* @constructor
+	*/
 	public ChatPanel(){
+		//add some padding to overall panel
 		this.wrapper.setBorder(new EmptyBorder(30, 30, 30, 30));
 		
-		button_container.add(sendMessage);
-		this.messagesList.setEnabled(false);
+		//add some padding to wrapper panels so it's easier to see their controls
 		this.yourMessageTxt.setBorder(new EmptyBorder(5, 5, 5, 5));
-			
+		button_container.setBorder(new EmptyBorder(10, 0, 5, 0));
+		your_message_panel.setBorder(new EmptyBorder(5, 0, 5, 0));
+		
+		//don't let them edit the messageList
+		this.messagesList.setEnabled(false);
+		
+		//add button to it's container (border layout to take up width)
+		button_container.add(sendMessage, BorderLayout.NORTH);
+		
+		//add your message controls to their wrapper
 		your_message_panel.add(yourMessageLbl, BorderLayout.NORTH);
 		your_message_panel.add(message_scroll, BorderLayout.CENTER);
 		your_message_panel.add(button_container, BorderLayout.SOUTH);
 		
-		
+		//add wrapper panels to main panel
 		this.wrapper.add(message_list_scroll,BorderLayout.CENTER);
 		this.wrapper.add(your_message_panel, BorderLayout.SOUTH);
 	}//end constructor
 	
 	
-	
+	/**
+	* Give the main panel for the chat screen
+	* @return JPanel the main panel
+	*/
 	public JPanel getPanel(){
 		return this.wrapper;
 	}//get method: getPanel
@@ -81,12 +92,19 @@ public class ChatPanel{
 	
 	/**
 	* get the message the user wants to send
+	* @return String the message they want to send
 	*/
 	public String getMessage(){
-	
-		return "";
+		return this.yourMessageTxt.getText();
 	}//end method: getMessage
 	
+	/**
+	* adds the new message to the list of messages
+	* @param String the message to add
+	*/
+	public void updateMessagesList(String msg){
+		this.messagesList.append(msg);
+	}//end method: updateMessagesList
 	
 	
 }//end class: ChatPanel
